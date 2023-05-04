8fd4e557d2d2313ba923abd8db2bac7d46218a45
Nikita Sidukov <nsidukov@icloud.com>
2023-05-04 17:01:58 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import VideoRecorder from "react-video-recorder";
+import { React, useState } from "react";
+import { Dialog, DialogTitle, DialogContent, Button } from "@material-ui/core";
+
+const App = () => {
+  const [openCam, setOpen] = useState(false);
+
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "10%",
+        border: "1px solid grey",
+        marginLeft: "30%",
+        marginRight: "30%",
+        padding: "5%",
+      }}
+    >
+      <Button
+        style={{
+          backgroundColor: "red",
+          fontSize: "25px",
+          borderRadius: "5em",
+        }}
+        onClick={() => setOpen(true)}
+      >
+        Запись
+      </Button>
+      <Dialog open={openCam} fullWidth maxWidth="sm">
+        <DialogTitle>
+          <div>
+            {/* Add new list */}
+            <Button onClick={() => setOpen(false)} style={{ float: "right" }}>
+              X
+            </Button>
+          </div>
+        </DialogTitle>
+        <DialogContent dividers>
+          <VideoRecorder
+            onRecordingComplete={(videoBlob) => {
+              // Do something with the video...
+              alert("videoBlob", videoBlob);
+            }}
+          />
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default App;
